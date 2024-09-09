4cc99a2e53d18703aed506bcab48abeb62401fec
Srinivasa chakilam <ch.bharadwaj183@gmail.com>
2024-09-09 10:53:59 -0400

    new changes
    Adding json file and writing code to the json data to ingest data from json to pysaprk using data frames

--- demo.py
+++ demo.py
@@ -1,2 +1,25 @@
-from pyspark.sql.types import StructField,StringType, IntegerType, StringType, DoubleType, StructType  
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StringType, IntegerType, DoubleType, StructType
 from pyspark.sql.functions import col, current_timestamp, lit
+
+# Create Spark session
+spark = SparkSession.builder \
+    .appName("PySpark Example") \
+    .getOrCreate()
+
+# Define the schema
+schema = StructType([
+    StructField("constructorId", IntegerType(), True),
+    StructField("constructorRef", StringType(), True),
+    StructField("name", StringType(), True),
+    StructField("nationality", StringType(), True),
+    StructField("url", StringType(), True)
+])
+
+# Load the JSON file with the specified schema
+df = spark.read.json(r"C:\Users\lenovo\Documents\GitHub\Pyspark\constructors.json", schema=schema)
+
+# Show the DataFrame
+
+df=df.withColumn("ingestion_date",current_timestamp())
+df.show()
